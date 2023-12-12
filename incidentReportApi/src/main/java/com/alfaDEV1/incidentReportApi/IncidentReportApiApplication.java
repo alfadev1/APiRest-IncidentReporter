340c8f756a276a1a7e3f7be50cc87a88be250d72
alfadev1 <alfarollano@gmail.com>
2023-12-12 01:38:36 -0300

    first commit

---
+++ incidentReportApi/src/main/java/com/alfaDEV1/incidentReportApi/IncidentReportApiApplication.java
@@ -0,0 +1,13 @@
+package com.alfaDEV1.incidentReportApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncidentReportApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncidentReportApiApplication.class, args);
+	}
+
+}
---
+++ incidentReportApi/src/test/java/com/alfaDEV1/incidentReportApi/IncidentReportApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alfaDEV1.incidentReportApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncidentReportApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
